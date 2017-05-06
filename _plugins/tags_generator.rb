e12f34f6364c89a10507ed94d85ab06f8c86fae9
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2017-05-06 16:45:56 -0400

    Rename tags_index layout to just tag

--- _plugins/tags_generator.rb
+++ _plugins/tags_generator.rb
@@ -6,7 +6,7 @@ module Jekyll
       @dir = dir
       @name = 'index.html'
       self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'tags_index.html')
+      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
       self.data['tag'] = tag
       tag_title_prefix = site.config['tag_title_prefix'] || 'Posts Tagged &ldquo;'
       tag_title_suffix = site.config['tag_title_suffix'] || '&rdquo;'
@@ -16,7 +16,7 @@ module Jekyll
   class TagGenerator < Generator
     safe true
     def generate(site)
-      if site.layouts.key? 'tags_index'
+      if site.layouts.key? 'tag'
         dir = site.config['tags_dir'] || 'tag'
         site.tags.keys.each do |tag|
           write_tag_index(site, File.join(dir, tag), tag)
